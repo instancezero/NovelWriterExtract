93da12fc7676f609ecaf5e040be9ce59ac086ae5
Alan Langford <instance1@gmail.com>
2026-08-09 21:14:45 -0400

    Refactor test data

--- test/test1b.php
+++ test/test1b.php
@@ -5,8 +5,8 @@ use Lib\NovelWriter\ExtractGrid;
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $extract = new ExtractGrid();
-$extract->setSourcePath(__DIR__ . '/projects/test1/ExtractTest');
-$extract->export(__DIR__ . '/test1b.ods', __DIR__ . '/test1b.json');
-$extract->export(__DIR__ . '/test1b.csv', __DIR__ . '/test1b.json');
-$extract->export(__DIR__ . '/test1b.html', __DIR__ . '/test1b.json');
-$extract->export(__DIR__ . '/test1b.xlsx', __DIR__ . '/test1b.json');
+$extract->setSourcePath(__DIR__ . '/testData/test1/ExtractTest');
+$extract->export(__DIR__ . '/results/test1b.ods', __DIR__ . '/test1b.json');
+$extract->export(__DIR__ . '/results/test1b.csv', __DIR__ . '/test1b.json');
+$extract->export(__DIR__ . '/results/test1b.html', __DIR__ . '/test1b.json');
+$extract->export(__DIR__ . '/results/test1b.xlsx', __DIR__ . '/test1b.json');
